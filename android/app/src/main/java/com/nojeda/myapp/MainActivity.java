8edf6d2e236c6e0f7e2d10534c7716b540913062
Néstor Ojeda González <nojeda@singularfactory.net>
2021-08-06 23:52:46 +0100

    ionic vue base project

---
+++ android/app/src/main/java/com/nojeda/myapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.nojeda.myapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/components/ExploreContainer.vue
+++ src/components/ExploreContainer.vue
@@ -1,17 +1,43 @@
 <template>
-  <div id="container">
-    <strong>{{ name }}</strong>
-    <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+  <div>
+    <h1>Geolocation</h1>
+    <p>Your location is:</p>
+    <p>Latitude: {{ loc.lat }}</p>
+    <p>Longitude: {{ loc.long }}</p>
+
+    <button @click="getCurrentPosition">
+      Get Current Location
+    </button>
   </div>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue';
+import { Geolocation } from '@capacitor/geolocation';
 
 export default {
   name: 'ExploreContainer',
   props: {
     name: String
-  }
+  },
+  setup() {
+    const loc = ref<{
+      lat: null | number;
+      long: null | number;
+    }>({
+      lat: null,
+      long: null,
+    });
+
+    const getCurrentPosition = async () => {
+      const pos = await Geolocation.getCurrentPosition();
+      loc.value = {
+        lat: pos.coords.latitude,
+        long: pos.coords.longitude,
+      };
+    };
+    return { getCurrentPosition, loc };
+  },
 }
 </script>
 
